880e561e29c90ee56b1cf44d9557a0294e9c56e8
22Crusader <89545245+22Crusader@users.noreply.github.com>
2024-11-20 16:43:21 +0200

    initial commit

---
+++ pages/details/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Organization details page
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="customers">
+        <div class="customers__header">
+            <span>
+                Searchbar
+            </span>
+            <span>
+                Filter1
+            </span>
+            <span>
+                Filter2
+            </span>
+            <span>
+                Filter3
+            </span>
+        </div>
+        <div class="customers__list">
+            <nuxt-link
+                v-for="org in 5"
+                class="customers__list-item"
+                :to="`/customers/details/${org}`"
+                :key="org"
+            >
+                Organization {{ org }}
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+    .customers {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        width: 100%;
+        padding: 0 2rem;
+
+        &__header {
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+        }
+
+        &__list {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            gap: 1rem;
+        }
+
+        &__list-item {
+            font-size: 1.25rem;
+            font-weight: 500;
+        }
+    }
+</style>
